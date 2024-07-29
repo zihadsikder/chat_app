b8ce44b88160f4f3d61236bbe7ddb7a44521e6fe
zihadsikder <zihadsikder.cse@gmail.com>
2024-07-29 16:11:24 +0600

    update chat services for sending & receiving message

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/models/message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -7,7 +8,7 @@ class ChatService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   /// get user stream
- /*
+  /*
  List<Map<String, dynamic> =
  [
  {
@@ -24,7 +25,7 @@ class ChatService {
 
   Stream<List<Map<String, dynamic>>> getUsersStream() {
     return _firestore.collection("Users").snapshots().map((snapshot) {
-      return snapshot.docs.map((doc){
+      return snapshot.docs.map((doc) {
         /// go though each individual user
         final user = doc.data();
 
@@ -34,16 +35,47 @@ class ChatService {
     });
   }
 
-/// send message
-Future<void> sendMessage(String receiverId, message) async {
+  /// send message
+  Future<void> sendMessage(String receiverID, message) async {
     /// get current user info
-  final String currentUserId = _auth.currentUser!.uid;
-  final String currentUserEmail = _auth.currentUser!.email!;
-  final Timestamp timestamp = Timestamp.now();
+    final String currentUserId = _auth.currentUser!.uid;
+    final String currentUserEmail = _auth.currentUser!.email!;
+    final Timestamp timestamp = Timestamp.now();
 
-  /// construct chat room id for the two user (sorted to ensure uniqueness)
-  /// add new message to database
-}
+    /// create a new message
+    Message newMessage = Message(
+      senderID: currentUserEmail,
+      senderEmail: currentUserId,
+      receiverID: receiverID,
+      message: message,
+      timestamp: timestamp,
+    );
 
-/// get message
+    /// construct chat room id for the two user (sorted to ensure uniqueness)
+    List<String> ids = [currentUserId, receiverID];
+    ids.sort(); //sort the ids (this ensure the chatroomID is the same for any 2 people)
+    String chatRoomID = ids.join('_');
+
+    /// add new message to database
+    await _firestore
+        .collection("chat_rooms")
+        .doc(chatRoomID)
+        .collection("message")
+        .add(newMessage.toMap());
+  }
+
+  /// get message
+  Stream<QuerySnapshot> getMessage(String userID, othersUserID) {
+    /// construct a chatroom ID for the two users
+    List<String> ids = [userID, othersUserID];
+    ids.sort();
+    String chatRoomID = ids.join('_');
+
+    return _firestore
+        .collection("chat_rooms")
+        .doc(chatRoomID)
+        .collection("message")
+        .orderBy("timestamp", descending: false)
+        .snapshots();
+  }
 }
